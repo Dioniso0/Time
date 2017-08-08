b10163bbc851bd0cb9867bdf8b983f244916cb45
Dioniso0 <30611028+Dioniso0@users.noreply.github.com>
2017-08-07 22:51:37 -0600

    Add files via upload
    Se ha subido la carpeta principal del proyecto a GitHub.

---
+++ Reloj/js/code.js
@@ -0,0 +1,23 @@
+		
+
+function Reloj () {
+
+	var Fecha = new Date();
+	var hora = Fecha.getHours();
+	var min = Fecha.getMinutes();
+	var seg = Fecha.getSeconds();
+
+	var recarga = setTimeout('Reloj()',500);
+
+	var x = document.getElementById('print');
+	x.innerHTML = hora + " : " + min + " : " + seg;
+}
+
+	
+
+	
+	
+
+	
+	
+	
