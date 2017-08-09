c36c17cda2213574ff0d8e5d58da9a53e40b7f6b
Dioniso0 <30611028+Dioniso0@users.noreply.github.com>
2017-08-08 18:55:58 -0600

    coments
    Agregue comentarios al codigo javascript.

--- Reloj/js/code.js
+++ Reloj/js/code.js
@@ -6,9 +6,10 @@ function Reloj () {
 	var hora = Fecha.getHours();
 	var min = Fecha.getMinutes();
 	var seg = Fecha.getSeconds();
-
+	//permite poder estar mostrando la funcion cada 500 mls
 	var recarga = setTimeout('Reloj()',500);
-
+	//estas variables toman los valores de los span en que se 
+	//muestra la hora
 	var x = document.getElementById('Hora');
 	var y = document.getElementById('Min');
 	var z = document.getElementById('Seg');
